13e5438a80146a7819fd8d8d7c0a574bc38fb620
Danteie <aleksandar.djuratovic93@gmail.com>
2022-07-06 12:08:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article className='home'>
+        <p>Hi, may name is</p>
+        <h1>Aleksandar Djuratovic</h1>
+        <p className='t-center'>Junior Front-end develpoer</p>
+      </article>
     </div>
   );
 }

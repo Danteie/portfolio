c87420afc43eb17aace304691f51ce04eb0a622a
Danteie <aleksandar.djuratovic93@gmail.com>
2022-07-07 13:25:39 +0200

    adding main menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import MainMenu from './Component/MainMenu/MainMenu'
 
 function App() {
   return (
     <div className="App">
+      <MainMenu/>
       <article className='home'>
         <p>Hi, may name is</p>
         <h1>Aleksandar Djuratovic</h1>
---
+++ src/Component/MainMenu/MainMenu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './MainMenu.css'
+
+function MainMenu() {
+  return (
+    <menu className="main-menu">
+        <a href="test">Skils</a>
+        <a href="test">Projects</a>
+        <a href="test">Contact</a>
+    </menu>
+  );
+}
+
+export default MainMenu;

ef2bfaab35e86b227f3e233490de8c449efb65e1
Danteie <aleksandar.djuratovic93@gmail.com>
2022-07-16 16:25:39 +0200

    adding contact page

--- src/Routes/Contact/Contact.js
+++ src/Routes/Contact/Contact.js
@@ -1,7 +1,22 @@
+import './Contact.css'
+
 export default function Contact() {
     return (
-        <article className='home'>
-          <h1>Contact</h1>
+        <article className='home contact'>
+          <div>
+            <p>NAME</p>
+            <h2>Aleksandar Djuratovic</h2>
+            <hr/>
+            <p>EMAIL</p>
+            <h2>aleksandr.djuratovic@gmail.com</h2>
+            <hr/>
+            <p>GIT HUB</p>
+            <h2>https://github.com/Danteie</h2>
+            <hr/>
+          </div>
+          <div>
+          <img  src="https://loremflickr.com/640/360" alt='project' className='project-image'/>
+          </div>
         </article>
     );
   }

7820b5fa70ab9e491401e1e319a8c6f142bf31e3
Danteie <aleksandar.djuratovic93@gmail.com>
2022-07-20 15:10:49 +0200

    styling skills page

--- src/Routes/Skils/Skils.js
+++ src/Routes/Skils/Skils.js
@@ -1,7 +1,21 @@
+import './Skills.css'
+
+import htmlimage from '.././../Assets/html5.png'
+import cssimage from '.././../Assets/css3.png'
+import jsimage from '.././../Assets/js.png'
+
+
+
 export default function Skils() {
+
     return (
-        <article className='home'>
-          <h1>SKILLS</h1>
+        <article className='skills'>
+          <div className='skills-icons'>
+            <img src={htmlimage} alt='html 5'/>
+            <img src={cssimage} alt='css 3'/>
+            <img src={jsimage} alt='java script'/>
+          </div>
+          
         </article>
     );
   }

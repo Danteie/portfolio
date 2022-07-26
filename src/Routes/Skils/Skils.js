3de87c4b0eaac95028d1e0971d8a2e7b4e025486
Danteie <aleksandar.djuratovic93@gmail.com>
2022-07-26 18:05:41 +0200

    add skills battery

--- src/Routes/Skils/Skils.js
+++ src/Routes/Skils/Skils.js
@@ -3,21 +3,31 @@ import './Skills.css'
 import htmlimage from '.././../Assets/html5.png'
 import cssimage from '.././../Assets/css3.png'
 import jsimage from '.././../Assets/js.png'
+import batery1 from '.././../Assets/batery1.png'
+import batery2 from '.././../Assets/batery2.png'
+import batery3 from '.././../Assets/batery3.png'
 
 
 
-export default function Skils() {
-
-    return (
-        <article className='skills'>
-          <div className='skills-icons'>
+export default function Skils() { 
+  return (
+      <article className='skills'>
+        <div className='skills-icons'>
+          <div>
             <img src={htmlimage} alt='html 5'/>
-            <img src={cssimage} alt='css 3'/>
-            <img src={jsimage} alt='java script'/>
+            <img src={batery1} alt='html 5'/>
           </div>
-          
-        </article>
-    );
-  }
+          <div>
+            <img src={cssimage} alt='css 3'/>
+            <img src={batery2} alt='html 5'/>
+          </div>
+          <div>
+            <img src={jsimage} alt='java script'/>
+            <img src={batery3} alt='html 5'/>
+          </div>
+        </div>  
+      </article>
+  );
+}
   
   
